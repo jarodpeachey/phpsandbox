e5965dbdbd2b0cac24b042040724cf66948d6dc5
Jarod Peachey <jwpeachey107@aol.com>
2019-08-15 13:10:57 -0400

    Finish include and require lesson

---
+++ Test Website/about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Website</title>
+    <link rel="stylesheet" href="https://jarodpeachey.github.io/breeze_css/breeze.css">
+  </head>
+  <body>
+    <?php include 'includes/header.php' ?>
+    <div class="center-text">
+      <h1>About</h1>
+    </div>
+    <?php include 'includes/footer.php' ?>
+  </body>
+</html>
---
+++ Test Website/includes/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Website</title>
+  </head>
+  <body>
+    <div class="container p-none">
+      <div class="navbar-content">
+        <div class="navbar-left">
+          <h3 class="m-none">My website</h3>
+        </div>
+        <div class="navbar-right">
+          <div class="menu">
+            <div class="menu-item">
+              <a href="index.php">
+                Home
+              </a>
+            </div>
+            <div class="menu-item">
+              <a href="about.php">
+                About
+              </a>
+            </div>
+            <div class="menu-item">
+              <a href="contact.php">
+                Contact
+              </a>
+            </div>
+          </div>      
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
